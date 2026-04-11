7ec03c8d4b9826980f9776850c7f14e2d924ae8d
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-04-11 11:18:26 +0300

    feat: implement restore functionality for deleted links in LinksTable

--- apps/frontend/src/components/homepage/index.tsx
+++ apps/frontend/src/components/homepage/index.tsx
@@ -4,9 +4,9 @@ import LinksTable from './LinksTable';
 
 const HomePage = () => {
   return (
-    <div className='w-full max-w-2xl p-4 flex-1 flex flex-col mt-4 mb-8 gap-4'>
-      <h1 className='text-2xl font-bold'>Welcome to the Short URL Service</h1>
-      <p className='text-muted-foreground'>Create and manage your short URLs with ease.</p>
+    <div className='w-full max-w-2xl p-4 flex-1 flex flex-col mt-4 mb-8 gap-6'>
+      <h1 className='text-2xl font-bold'>URL Shortener</h1>
+      <p className='text-muted-foreground -mt-4'>Create and manage your short URLs</p>
       <AuthGuard>
         <CreateLinkForm />
         <LinksTable />
