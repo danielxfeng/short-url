dc6f3288a150df9b8cad73972f3535f38c7db496
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2026-04-12 09:52:13 +0300

    feat: add custom ErrorPage component for error boundaries

---
+++ apps/frontend/src/components/ErrorPage.tsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div className='w-full min-h-dvh flex flex-col items-center justify-center'>
+      <h1 className='text-2xl font-bold mb-4'>Error</h1>
+      <p className='text-muted-foreground'>An error occurred, please try again</p>
+    </div>
+  );
+};
+
+export default ErrorPage;
